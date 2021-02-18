b4db37f02b19975812a1c062d0f07fafeb50b222
Dex Fitch <fitch.dexter@gmail.com>
2021-02-17 19:14:09 -0800

    commit tree d75d1a0f86e894ba3b072d6022d9dd33eddccdd1

--- src/App.js
+++ src/App.js
@@ -1,1 +1,27 @@
-// create your App component here
+import React, { Component } from 'react'
+
+export default class App extends Component {
+  state = {
+    peopleInSpace: []
+  }
+
+  componentDidMount() {
+    fetch('http://api.open-notify.org/astros.json')
+      .then(res => res.json())
+      .then(data => {
+        this.setState({
+          peopleInSpace: data.people
+        })
+      })
+  }
+
+  render() {
+    return(
+      <div>
+        <ul style={{listStyleType: "none", padding: 0, margin: "10px 0 0 10px"}}>
+          {this.state.peopleInSpace.map(spacefolk => <li key="{spacefolk.id}" style={{marginBottom: "5px"}}>{spacefolk.name}</li>)}
+        </ul>
+      </div>
+    )
+  }
+}
